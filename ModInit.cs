a5a38d434c957779cfa8ba44487df2ce5496f6d8
HazardousNose <hazardousnose@gmail.com>
2023-07-05 19:45:32 -0500

    initial commit

---
+++ ModInit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace NoTraderProtection
+{
+    public class ModApi : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            Log.Out(" Loading Patch: " + GetType());
+
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
